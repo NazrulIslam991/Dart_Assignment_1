f83b57197131d31273d0ab4af142f1b9b3259bd2
mdnaz <nazrulislamnayon991@gmail.com>
2025-03-29 11:18:20 +0600

    complete

--- bin/dart_assignment_1.dart
+++ bin/dart_assignment_1.dart
@@ -1,3 +1,39 @@
+
+
+// Main Class
 void main(){
-  print("nayon");
+  Car ob = Car();
+  ob.speed=80;
+  ob.move();
+}
+
+
+
+
+// abstract class of vehicle
+abstract class Vehicle {
+  int? _speed;
+
+  void move();
+
+  set speed(int speed) {
+    _speed = speed;
+  }
+
+  int? get Speed {
+    return _speed;
+  }
+}
+
+
+
+
+// Car class
+class Car extends Vehicle{
+
+  @override
+  move() {
+    print("The car is moving at $Speed km/h");
+  }
+
 }
